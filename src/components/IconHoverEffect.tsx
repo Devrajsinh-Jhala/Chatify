3bd72851b6d21e1476083b3cae9610052b29840a
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-06-07 01:20:49 +0530

    Ignore type error

--- src/components/IconHoverEffect.tsx
+++ src/components/IconHoverEffect.tsx
@@ -8,17 +8,17 @@ type Props = {
 };
 
 const IconHoverEffect = ({ children, red = false, blue = false }: Props) => {
-  var colorClasses = red
+  let colorClasses = red
     ? "outline-red-400 hover:bg-red-200 group-hover-bg-red-200 group-focus-visible:bg-red-200 focus-visible-bg-red-200"
     : "outline-gray-400 hover:bg-gray-200 dark:hover:text-gray-800 group-hover-bg-gray-200 group-focus-visible:bg-gray-200 focus-visible-bg-gray-200";
 
-  var colorClasses = blue
+  let bookmarkClasses = blue
     ? "outline-blue-400 hover:bg-blue-200 group-hover-bg-blue-200 group-focus-visible:bg-blue-200 focus-visible-bg-blue-200"
     : "outline-gray-400 hover:bg-gray-200 dark:hover:text-gray-800 group-hover-bg-gray-200 group-focus-visible:bg-gray-200 focus-visible-bg-gray-200";
 
   return (
     <div
-      className={`rounded-full p-2 transition-colors duration-200  ${colorClasses}`}
+      className={`rounded-full p-2 transition-colors duration-200  ${colorClasses} ${bookmarkClasses}`}
     >
       {children}
     </div>
